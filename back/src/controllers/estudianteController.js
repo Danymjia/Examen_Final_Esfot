7557b5361aaea36b7762a45c5cb83252ba73efa5
mercysperugachi <mercysperugachis@gmail.com>
2026-02-26 09:26:27 -0500

    creación del CRUD de estudiante

--- back/src/controllers/estudianteController.js
+++ back/src/controllers/estudianteController.js
@@ -14,6 +14,44 @@ const crearEstudiante = async (req, res) => {
     }
 }
 
+const verEstudiante = async (req, res) => {
+    try{
+        const {id} = req.params
+        const estudianteBDD = await Estudiante.findById(id)
+        if(!estudianteBDD) return res.status(404).json({msg:"Estudiante no encontrado"})
+        res.status(200).json({msg:"Estudiante encontrado", estudiante:estudianteBDD})
+    } catch (error) {
+        res.status(500).json({msg:"Error al ver el estudiante", error:error.message})
+    }
+}
+
+const actualizarEstudiante = async (req, res) => {
+    try {
+        const {id} = req.params 
+        const estudianteBDD = await Estudiante.findById(id)
+        if(!estudianteBDD) return res.status(404).json({msg:"Estudiante no encontrado"})
+        const estudianteActualizado = await Estudiante.findByIdAndUpdate(id, req.body, {new:true})
+        res.status(200).json({msg:"Estudiante actualizado correctamente", estudiante:estudianteActualizado})
+    } catch (error) {
+        res.status(500).json({msg:"Error al actualizar el estudiante", error:error.message})
+    }
+}
+
+const eliminarEstudiante = async (req, res) => {   
+    try {   
+        const {id} = req.params 
+        const estudianteBDD = await Estudiante.findById(id)       
+        if(!estudianteBDD) return res.status(404).json({msg:"Estudiante no encontrado"})
+        await Estudiante.findByIdAndDelete(id)
+        res.status(200).json({msg:"Estudiante eliminado correctamente"})
+    } catch (error) {
+        res.status(500).json({msg:"Error al eliminar el estudiante", error:error.message})
+    }
+}
+
 export {
-    crearEstudiante
+    crearEstudiante,
+    verEstudiante,
+    actualizarEstudiante,
+    eliminarEstudiante
 }
